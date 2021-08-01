75417803f843dda9faab1844833547901a0baf7c
MollyK-A <molly.k.andersson@live.se>
2021-08-01 16:54:23 +0200

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,105 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'picocms/pico-composer',
+  ),
+  'versions' => 
+  array (
+    'erusev/parsedown' => 
+    array (
+      'pretty_version' => '1.8.0-beta-7',
+      'version' => '1.8.0.0-beta7',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe7a50eceb4a3c867cc9fa9c0aa906b1067d1955',
+    ),
+    'erusev/parsedown-extra' => 
+    array (
+      'pretty_version' => '0.8.0',
+      'version' => '0.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '32f6e957286bf9ff98d07ca924a18299461acc8f',
+    ),
+    'mos/cimage' => 
+    array (
+      'pretty_version' => 'v0.8.1',
+      'version' => '0.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '024905676145ee8a000fb0b6735ce6f8ba98f427',
+    ),
+    'picocms/composer-installer' => 
+    array (
+      'pretty_version' => 'v1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b5036c83aa091ed76e2a76ed9335885f95a7db7',
+    ),
+    'picocms/pico' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '232a90bdf8dac5c2710c1a4b27a397bf28f9a75c',
+    ),
+    'picocms/pico-composer' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'picocms/pico-deprecated' => 
+    array (
+      'pretty_version' => 'v2.1.0',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8693b27f906a982409a520a41710bb5c95218882',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.18.1',
+      'version' => '1.18.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c302646f6efc070cd46856e600e5e0684d6b454',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v2.8.52',
+      'version' => '2.8.52.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '02c1859112aa779d9ab394ae4f3381911d84052b',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.43.1',
+      'version' => '1.43.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2311602f6a208715252febe682fa7c38e56a3373',
+    ),
+  ),
+);
